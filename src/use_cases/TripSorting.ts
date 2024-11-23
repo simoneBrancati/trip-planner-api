4a12595553e75290a7e51020184657886465e95f
Simone Brancati <simonebrancati90@gmail.com>
2024-11-23 18:47:28 +0100

    Added main function to get sorted trips

--- src/use_cases/TripSorting.ts
+++ src/use_cases/TripSorting.ts
@@ -1,24 +1,58 @@
-import { SortingStrategy, Trip } from "../entities/Trip";
+import { IATACode } from "../entities/IATACodes";
+import { SortingFunction, SortingStrategy, Trip } from "../entities/Trip";
+import { TripGateway } from "../gateways/TripGateway";
+
+/**
+ * Retrieves and sorts trips by the specified strategy.
+ * @param origin - Origin IATA code.
+ * @param destination - Destination IATA code.
+ * @param sortBy - Sorting strategy ("cheapest" or "fastest").
+ * @param tripGateway - Gateway for fetching trips.
+ * @returns An array of trips sorted by the specified strategy.
+ */
+export const getSortedTrips = async (
+  origin: IATACode,
+  destination: IATACode,
+  sortBy: SortingStrategy,
+  tripGateway: TripGateway,
+): Promise<Trip[]> => {
+  // Validation
+  if (!origin || !destination) {
+    throw new Error("Origin and destination must be provided.");
+  }
+
+  if (!["cheapest", "fastest"].includes(sortBy)) {
+    throw new Error(`Invalid sorting strategy "${sortBy}".`);
+  }
+
+  // Fetch trips from the gateway
+  const trips = await tripGateway.fetchTrips(origin, destination);
+
+  // Sorting
+  const sortFunction = getSortingFunction(sortBy);
+  return sortFunction(trips);
+};
 
 /**
  * Sorts trips based on the specified sorting strategy.
  * @param trips - The array of Trip objects to sort.
  * @param sortingStrat - The sorting strategy to use ("cheapest" or "fastest").
- * @returns A new array of Trip objects sorted by the specified strategy.
+ * @returns An array of trips sorted by the specified strategy.
  */
-export const sortTrips = (
-  trips: Trip[],
+export const getSortingFunction = (
   sortingStrat: SortingStrategy,
-): Trip[] => {
-  if (sortingStrat === "cheapest") {
-    return sortByCheapest(trips);
+): SortingFunction => {
+  const strategies: Record<SortingStrategy, SortingFunction> = {
+    cheapest: sortByCheapest,
+    fastest: sortByFastest,
+  };
+
+  const sortFunction = strategies[sortingStrat];
+  if (!sortFunction) {
+    throw new Error(`Sorting strategy "${sortingStrat}" is not supported.`);
   }
 
-  if (sortingStrat === "fastest") {
-    return sortByFastest(trips);
-  }
-
-  return trips;
+  return sortFunction;
 };
 
 /**
