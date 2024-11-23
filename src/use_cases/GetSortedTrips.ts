5de8df9358e98c35ea87bbe22ab0e53e3091bef3
Simone Brancati <simonebrancati90@gmail.com>
2024-11-23 18:59:56 +0100

    Refactor use cases and tests

---
+++ src/use_cases/GetSortedTrips.ts
@@ -0,0 +1,35 @@
+import { IATACode } from "../entities/IATACodes";
+import { SortingStrategy, Trip } from "../entities/Trip";
+import { TripGateway } from "../gateways/TripGateway";
+import { getSortingFunction } from "./TripSorting";
+
+/**
+ * Retrieves and sorts trips by the specified strategy.
+ * @param origin - Origin IATA code.
+ * @param destination - Destination IATA code.
+ * @param sortBy - Sorting strategy ("cheapest" or "fastest").
+ * @param tripGateway - Gateway for fetching trips.
+ * @returns An array of trips sorted by the specified strategy.
+ */
+export const getSortedTrips = async (
+  origin: IATACode,
+  destination: IATACode,
+  sortBy: SortingStrategy,
+  tripGateway: TripGateway,
+): Promise<Trip[]> => {
+  // Validation
+  if (!origin || !destination) {
+    throw new Error("Origin and destination must be provided.");
+  }
+
+  if (!["cheapest", "fastest"].includes(sortBy)) {
+    throw new Error(`Invalid sorting strategy "${sortBy}".`);
+  }
+
+  // Fetch trips from the gateway
+  const trips = await tripGateway.fetchTrips(origin, destination);
+
+  // Sorting
+  const sortFunction = getSortingFunction(sortBy);
+  return sortFunction(trips);
+};
--- src/use_cases/TripSorting.ts
+++ src/use_cases/TripSorting.ts
@@ -1,37 +1,4 @@
-import { IATACode } from "../entities/IATACodes";
 import { SortingFunction, SortingStrategy, Trip } from "../entities/Trip";
-import { TripGateway } from "../gateways/TripGateway";
-
-/**
- * Retrieves and sorts trips by the specified strategy.
- * @param origin - Origin IATA code.
- * @param destination - Destination IATA code.
- * @param sortBy - Sorting strategy ("cheapest" or "fastest").
- * @param tripGateway - Gateway for fetching trips.
- * @returns An array of trips sorted by the specified strategy.
- */
-export const getSortedTrips = async (
-  origin: IATACode,
-  destination: IATACode,
-  sortBy: SortingStrategy,
-  tripGateway: TripGateway,
-): Promise<Trip[]> => {
-  // Validation
-  if (!origin || !destination) {
-    throw new Error("Origin and destination must be provided.");
-  }
-
-  if (!["cheapest", "fastest"].includes(sortBy)) {
-    throw new Error(`Invalid sorting strategy "${sortBy}".`);
-  }
-
-  // Fetch trips from the gateway
-  const trips = await tripGateway.fetchTrips(origin, destination);
-
-  // Sorting
-  const sortFunction = getSortingFunction(sortBy);
-  return sortFunction(trips);
-};
 
 /**
  * Sorts trips based on the specified sorting strategy.
