956190de8c400787304d07af83e604b8b6f1a70d
Simone Brancati <simonebrancati90@gmail.com>
2024-11-24 21:38:28 +0100

    Added the cache gateway to the controller

--- src/controllers/TripController.ts
+++ src/controllers/TripController.ts
@@ -3,6 +3,7 @@ import { getSortedTrips } from "../use_cases/GetSortedTrips";
 import { HttpTripGateway } from "../gateways/HttpGateway";
 import { IATACode } from "../entities/IATACodes";
 import { SortingStrategy } from "../entities/Trip";
+import { RedisCacheGateway } from "../gateways/RedisGateway";
 
 /**
  * Controller function that connects the business core logic to the express application framework.
@@ -19,12 +20,14 @@ export const getTrips = async (
   const { origin, destination, sort_by } = req.query;
   try {
     const tripGateway = HttpTripGateway();
+    const cacheGateway = RedisCacheGateway();
 
     const trips = await getSortedTrips(
       origin as IATACode,
       destination as IATACode,
       sort_by as SortingStrategy | undefined,
       tripGateway,
+      cacheGateway,
     );
 
     res.status(200).json(trips);
