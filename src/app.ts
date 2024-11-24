8142be4d4afe0a71aec0c127dc592b2e7429c591
Simone Brancati <simonebrancati90@gmail.com>
2024-11-24 10:09:39 +0100

    Added logging in app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import express, { Express } from "express";
 import router from "./frameworks/express/router";
 import loggingMiddleware from "./frameworks/express/middlewares/logger";
-
 import dotenv from "dotenv";
+import logger from "./utils/logger";
+
 dotenv.config();
 
 const app: Express = express();
@@ -13,7 +14,7 @@ app.use(loggingMiddleware);
 app.use("/", router);
 
 app.listen(process.env.APP_PORT, () => {
-  console.info(`Listening on port ${process.env.APP_PORT}`);
+  logger.info(`Listening on port ${process.env.APP_PORT}`);
 });
 
 export default app;
