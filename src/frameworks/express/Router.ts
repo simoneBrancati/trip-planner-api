3690d0cd8e27724556b9bd47abfd659b71b4461d
Simone Brancati <simonebrancati90@gmail.com>
2024-11-25 22:28:47 +0100

    Added new express route to save trips

--- src/frameworks/express/Router.ts
+++ src/frameworks/express/Router.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { getTrips } from "../../controllers/TripController";
-import { getTripsValidator } from "./middlewares/TripValidators";
+import { getTrips, saveTrip } from "../../controllers/TripController";
+import {
+  getTripsValidator,
+  saveTripValidator,
+} from "./middlewares/TripValidators";
 
 const router = express.Router();
 
 router.get("/trips", getTripsValidator, getTrips);
+router.post("/trip", saveTripValidator, saveTrip);
 
 export default router;
