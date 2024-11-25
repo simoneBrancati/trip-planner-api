2a0387a9701f2a4b994921b932827c671b74c8b1
Simone Brancati <simonebrancati90@gmail.com>
2024-11-25 22:40:19 +0100

    Added controller to save trips

--- src/controllers/TripController.ts
+++ src/controllers/TripController.ts
@@ -1,12 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { getSortedTrips } from "../use_cases/GetSortedTrips";
+import { saveTrip as logicSaveTrip } from "../use_cases/SaveTrip";
 import { HttpTripGateway } from "../gateways/HttpGateway";
 import { IATACode } from "../entities/IATACodes";
-import { SortingStrategy } from "../entities/Trip";
+import { SortingStrategy, Trip } from "../entities/Trip";
 import { RedisCacheGateway } from "../gateways/RedisGateway";
+import { MongoRepositoryGateway } from "../gateways/MongoDbGateway";
 
 /**
  * Controller function that connects the business core logic to the express application framework.
+ * Get trips from a 3rd party API and return it to the user.
  *
  * @param req Express Request object.
  * @param res Express Response object.
@@ -31,6 +34,34 @@ export const getTrips = async (
     );
 
     res.status(200).json(trips);
+  } catch (err) {
+    next(err);
+  }
+};
+
+/**
+ * Controller function that connects the business core logic to the express application framework.
+ * Save a trip to the repository.
+ *
+ * @param req Express Request object.
+ * @param res Express Response object.
+ * @param next Express Next function.
+ */
+export const saveTrip = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  const { trip } = req.query;
+  try {
+    const repositoryGateway = MongoRepositoryGateway();
+
+    const savedTrip = await logicSaveTrip(
+      trip as Trip | undefined,
+      repositoryGateway,
+    );
+
+    res.status(200).json({ trip: savedTrip });
   } catch (err) {
     next(err);
   }
