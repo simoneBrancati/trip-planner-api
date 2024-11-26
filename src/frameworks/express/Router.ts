a42ce4fa5744afe05dae9de0fd1e1c5ce6aadf4e
Simone Brancati <sbrancati@argosoft.it>
2024-11-26 16:38:54 +0100

    Fix cors options for preflight request

--- src/frameworks/express/Router.ts
+++ src/frameworks/express/Router.ts
@@ -14,13 +14,18 @@ import { getGithubPageCors } from "./Cors";
 
 const router = express.Router();
 
-const getCors = getGithubPageCors(["GET"]);
-const postCors = getGithubPageCors(["POST", "OPTIONS"]);
-const deleteCors = getGithubPageCors(["DELETE", "OPTIONS"]);
+router.get("/trips", getGithubPageCors(["GET"]), getTripsValidator, getTrips);
 
-router.get("/trips", getCors, getTripsValidator, getTrips);
-router.post("/my-trip", postCors, express.json(), saveTripValidator, saveTrip);
-router.get("/my-trips", getCors, listSavedTrips);
-router.delete("/my-trip", deleteCors, deleteTripValidator, deleteTrip);
+const myTripCors = getGithubPageCors(["POST", "OPTIONS", "DELETE"]);
+router.options("/my-trip", myTripCors);
+router.post(
+  "/my-trip",
+  myTripCors,
+  express.json(),
+  saveTripValidator,
+  saveTrip,
+);
+router.delete("/my-trip", myTripCors, deleteTripValidator, deleteTrip);
+router.get("/my-trips", getGithubPageCors(["GET"]), listSavedTrips);
 
 export default router;
