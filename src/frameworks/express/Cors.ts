18dfa510b1376bd262a268801964cbff69fd05b3
Simone Brancati <sbrancati@argosoft.it>
2024-11-26 16:15:47 +0100

    Fix cors path

--- src/frameworks/express/Cors.ts
+++ src/frameworks/express/Cors.ts
@@ -4,7 +4,7 @@ export const getGithubPageCors = (
   methods: Array<"GET" | "POST" | "PUT" | "DELETE" | "OPTIONS">,
 ) => {
   const options: CorsOptions = {
-    origin: "https://simonebrancati.github.io/trip-planner-api",
+    origin: "https://simonebrancati.github.io",
     methods,
   };
   return cors(options);
